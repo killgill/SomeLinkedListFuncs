b64ab4b335f72a81d9016e70f71273ede54b04e3
killgill <gill@cooper.edu>
2017-11-30 10:48:56 -0800

    added files

---
+++ cs455-ec/ecListFuncs.cpp
@@ -0,0 +1,41 @@
+/*  Name:
+*  USC NetID:
+*  CS 455 Fall 2017
+*
+*  See ecListFuncs.h for specification of each function.
+*/
+
+#include <iostream>
+
+#include <cassert>
+
+#include "ecListFuncs.h"
+
+using namespace std;
+
+
+int numRuns(ListType list) {
+
+    return 0;  // stub code to get it to compile
+
+}
+
+
+
+ListType reverseCopy(ListType list) {
+
+    return NULL;  // stub code to get it to compile
+
+}
+
+
+
+void removeMiddleElmt(ListType &list) {
+
+}
+
+
+
+void splitOn(ListType &list, int splitVal, ListType &a, ListType &b) {
+
+}
